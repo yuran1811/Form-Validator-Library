51d11711c1c8665449a711f19bb861f4768172b8
yuran1811 <trieuvanbd123@gmail.com>
2022-01-07 13:57:33 +0700

    Updăte

--- assets/render.js
+++ assets/render.js
@@ -2,49 +2,80 @@ const $ = document.querySelector.bind(document);
 const $$ = document.querySelectorAll.bind(document);
 
 const main = $('.main');
+const DATA_FROM_LOCAL = JSON.parse(localStorage.getItem('data'));
 
-const form1 = {
-	name: 'register-form',
-	method: 'POST',
-	action: '',
+const rerender = (data) => {
+	localStorage.setItem('data', JSON.stringify(data));
 
-	heading: 'SIGN UP',
-	description: 'Have Great Time With Us!!!',
-	elements: [
-		{
-			name: 'Full name',
-			id: 'fullname',
-			type: 'text',
-			placeholder: 'Ex: Yuran',
-			rules: 'required',
-		},
-		{
-			name: 'Email',
-			id: 'email',
-			type: 'text',
-			placeholder: 'Ex: email@domain.com',
-			rules: 'required|email',
-		},
-		{
-			name: 'Password',
-			id: 'password',
-			type: 'password',
-			placeholder: 'Type your password',
-			rules: 'required|min:6',
-		},
-		{
-			name: 'Confirm your password',
-			id: 'password_confirmation"',
-			type: 'password',
-			placeholder: 'Confirm your password',
-			rules: 'required|confirmed',
-		},
-	],
-	submitTitle: 'Sign Up',
+	const html = `
+		<div class="info__container">
+			<div class="info__item info__name">
+				<span class="info__title">Your name:</span>
+				<div class="info__data">${data.fullname}</div>
+			</div>
+			<div class="info__item info__email">
+				<span class="info__title">Your email:</span>
+				<div class="info__data">${data.email}</div>
+			</div>
+			<div class="info__item info__password">
+				<span class="info__title">Your password:</span>
+				<div class="info__data">${data.password}</div>
+			</div>
+
+			<button>Clear Data</button>
+		</div>
+	`;
+	main.innerHTML = html;
+
+	const clearBtn = $('button');
+	clearBtn.onclick = () => {
+		localStorage.clear();
+		render();
+	};
 };
+const render = () => {
+	const form1 = {
+		name: 'register-form',
+		method: 'POST',
+		action: '',
 
-const eleHTML = form1.elements.map(
-	(item) => `
+		heading: 'SIGN UP',
+		description: 'Have Great Time With Us!!!',
+		elements: [
+			{
+				name: 'Full name',
+				id: 'fullname',
+				type: 'text',
+				placeholder: 'Ex: Yuran',
+				rules: 'required',
+			},
+			{
+				name: 'Email',
+				id: 'email',
+				type: 'text',
+				placeholder: 'Ex: email@domain.com',
+				rules: 'required|email',
+			},
+			{
+				name: 'Password',
+				id: 'password',
+				type: 'password',
+				placeholder: 'Type your password',
+				rules: 'required|min:6',
+			},
+			{
+				name: 'Confirm your password',
+				id: 'password_confirmation"',
+				type: 'password',
+				placeholder: 'Confirm your password',
+				rules: 'required|confirmed',
+			},
+		],
+		submitTitle: 'Sign Up',
+	};
+
+	const eleHTML = form1.elements.map(
+		(item) => `
 		<div class="form-group">
 			<label for="${item.id}" class="form-label">${item.name}</label>
 			<input
@@ -57,8 +88,8 @@ const eleHTML = form1.elements.map(
 			/>
 			<span class="form-message"></span>
 		</div>`
-);
-const htmls = `
+	);
+	const htmls = `
 	<form action="" method="${form1.method}" class="form" id="${form1.name}">
 		<h3 class="heading">${form1.heading}</h3>
 		<p class="desc">${form1.description}</p>
@@ -66,4 +97,8 @@ const htmls = `
 		${eleHTML}
 		<button class="form-submit">${form1.submitTitle}</button>
 	</form>`;
-main.innerHTML = htmls;
+	main.innerHTML = htmls;
+};
+
+if (DATA_FROM_LOCAL) rerender(DATA_FROM_LOCAL);
+else render();
